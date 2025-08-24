e0e85e5e04242de003b09e059b763215b50c0ccb
shyam086 <patelshyan693@gmail.com>
2025-08-24 23:44:16 +0530

    Uploaded Python tasks

---
+++ ASSIGNMENT 1/Task_1.py
@@ -0,0 +1,19 @@
+# 1.  Takes two numbers as input from the user.
+
+num_1 = (int(input("Enter the first number: ")))
+num_2 = (int(input("Enter the second number: ")))
+
+# 2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+
+
+print("Addition:", num_1 + num_2)
+
+print("Subtraction:", num_1 - num_2)
+
+print("Multiplication:", num_1 * num_2)
+
+print("Division:", num_1 / num_2)
---
+++ ASSIGNMENT 1/Task_2.py
@@ -0,0 +1,12 @@
+# 1.  Takes a user's first name and last name as input.
+
+first_name = (input("Enter your first name:"))
+last_name = (input("Enter your last name:"))
+
+# 2.  Concatenates the first name and last name into a full name.
+
+full_name = first_name + " " + last_name
+
+# 3.  Prints a personalized greeting message using the full name.
+
+print ("Hello ," + full_name + "! Welcom to the python program.")
